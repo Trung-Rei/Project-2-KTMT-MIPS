fcb1532cd404e7b9a1a8bc5d54949302bb59b541
123prob7 <truongnguyenvan9a9@gmail.com>
2020-06-11 10:55:19 +0700

    another high effort mess

--- QuickSort.asm
+++ QuickSort.asm
@@ -1,125 +1,160 @@
 .data
 fin: .asciiz "input_sort.txt"
 fout: .asciiz "output_sort.txt"
-buffer: .space 1024
+arr: .word 4 20 50 35 18 67
 .text
 .globl main
 main:
-	###############################################################
-	# Open (for writing) a file that does not exist
-	li   $v0, 13       # system call for open file
-	la   $a0, fin      # output file name
-	li   $a1, 0        # Open for writing (flags are 0: read, 1: write)
-	li   $a2, 0        # mode is ignored
-	syscall            # open a file (file descriptor returned in $v0)
-	move $s6, $v0      # save the file descriptor 
-	
-	#read from file
-	li   $v0, 14       # system call for read from file
-	move $a0, $s6      # file descriptor 
-	la   $a1, buffer   # address of buffer to which to read
-	li   $a2, 1024     # hardcoded buffer length
-	syscall            # read from file
+	la $s0, arr
+	li $a0, 0	#low = 0
+	li $a1, 5	#high = n - 1
+	jal quickSort
+	li $t0, 0	#i = 0
+loop1:
+	bge     $t0, 6, exit
 
-	# Close the file 
-	li   $v0, 16       # system call for close file
-	move $a0, $s6      # file descriptor to close
-	syscall            # close file
+	# load word from addrs and goes to the next addrs
+	lw      $t2, 0($s0)
+	addi    $s0, $s0, 4
+
+	# syscall to print value
+	li      $v0, 1      
+	move    $a0, $t2
+	syscall
+	# optional - syscall number for printing character (space)
+	li      $a0, 32
+	li      $v0, 11  
+	syscall
+
+	#increment counter
+	addi    $t0, $t0, 1
+	j      loop1
+
+exit:
+	li $v0, 10
+	syscall
 
 
-	#Array = $s0
-	#partition(low = $a0, high = $a1) -------pivot = $t0
-	#return left in $v0
-	partitionFunc:
-		#pivot = a[high]
-		sll $t1, $a1, 2		#t1 = high * size of(int)
-		addu $t1, $t1, $s0	#t1 = &arr[high]
-		lw $t0, 0($t1)		#t0 = arr[high]
-		#left ($t1) = low
-		move $t1, $a0
-		#right ($t2) = high - 1
-		subi $t2, $a1, 1
 
-	whileTrue:
+#Array = $s0
+#partition(low = $a0, high = $a1)
+#return left in $v0
+partitionFunc:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	sll $t1, $a1, 2		#t1 = high * size of(int)
+	addu $t1, $t1, $s0	#t1 = &arr[high]
+	lw $t0, 0($t1)		#t0 = arr[high] = pivot
+	#left ($t1) = low
+	move $t1, $a0
+	#right ($t2) = high - 1
+	subi $t2, $a1, 1
 
-		leftCondition1:
-			ble $t1, $t2, leftCondition2	#left <= right
-			j rightCondition1
-			leftCondition2:
-			sll $t3, $t1, 2
-			add $t3, $t3, $s0
-			lw $t3, 0($t3)		#arr[left]
-			blt $t3, $t0, leftIncr
-			j rightCondition1
-			leftIncr:	#left++
+whileTrue:
+
+	leftCondition1:
+		ble $t1, $t2, leftCondition2	#left <= right
+		j rightCondition1
+	leftCondition2:
+		sll $t3, $t1, 2
+		add $t3, $t3, $s0
+		lw $t3, 0($t3)		#arr[left]
+		blt $t3, $t0, leftIncr #arr[left] < pivot
+		j rightCondition1
+		leftIncr:	#left++
 			addi $t1, $t1, 1
 			j leftCondition1
 
-		rightCondition1:
-			bge $t2, $t1, rightCondition2
-			j nextCommand
-			rightCondition2:
-			sll $t3, $t2, 2
-			add $t3, $t3, $s0
-			lw $t3, 0($t3)		#arr[right]
-			bgt $t3, $t0, rightDecr		#arr[right] > pivot
-			j nextCommand
-			rightDecr:
-			subi $t2, $t2, 1
-			j rightCondition1
+	rightCondition1:
+		bge $t2, $t1, rightCondition2	#right >= left
+		j nextCommand
+	rightCondition2:
+		sll $t3, $t2, 2
+		add $t3, $t3, $s0
+		lw $t3, 0($t3)		#arr[right]
+		bgt $t3, $t0, rightDecr		#arr[right] > pivot
+		j nextCommand
+		rightDecr:	#right--
+		subi $t2, $t2, 1
+		j rightCondition1
 
-		nextCommand:
-			bge $t1, $t2, leftGreaterThanRight
-			# store arr[left] in $a2; arr[right] in $a3
-			sll $t3, $t1, 2
-			add $t3, $t3, $s0
-			lw $a2, 0($t3)
-			sll $t3, $t2, 2
-			add $t3, $t3, $s0
-			lw $a3, 0($t3)
-			jal swap
-			#left++
-			addi $t1, $t1, 1
-			#right--
-			$subi $t2, $t2, 1
-			j whileTrue
+	nextCommand:
+		bge $t1, $t2, leftGreaterThanRight
+		# store arr[left] in $a2; arr[right] in $a3
+		sll $t3, $t1, 2
+		add $t3, $t3, $s0
+		lw $a2, 0($t3)
+		sll $t3, $t2, 2
+		add $t3, $t3, $s0
+		lw $a3, 0($t3)
+		jal swap
+		# update in $s0
+		sll $t3, $t1, 2
+		add $t3, $t3, $s0
+		sw $a2, 0($t3)
+		sll $t3, $t2, 2
+		add $t3, $t3, $s0
+		sw $a3, 0($t3)
+		#left++
+		addi $t1, $t1, 1
+		#right--
+		subi $t2, $t2, 1
+		j whileTrue
 
-		leftGreaterThanRight:
-			# store arr[left] in $a2; arr[high] in $a3
-			sll $t3, $t1, 2
-			add $t3, $t3, $s0
-			lw $a2, 0($t3)
-			sll $t3, $a1, 2
-			add $t3, $t3, $s0
-			lw $a3, 0($t3)
-			jal swap
-			#return left
-			move $v0, $t1
-			jr $ra
-
-	swap: #swap($a2, $a3)
-		move $t9, $a2
-		move $a2, $a3
-		move $a3, $t9
+	leftGreaterThanRight:
+		# store arr[left] in $a2; arr[high] in $a3
+		sll $t3, $t1, 2
+		add $t3, $t3, $s0
+		lw $a2, 0($t3)
+		sll $t3, $a1, 2
+		add $t3, $t3, $s0
+		lw $a3, 0($t3)
+		jal swap
+		# update in $s0
+		sll $t3, $t1, 2
+		add $t3, $t3, $s0
+		sw $a2, 0($t3)
+		sll $t3, $a1, 2
+		add $t3, $t3, $s0
+		sw $a3, 0($t3)
+		#return left
+		move $v0, $t1
+		lw $ra, 0($sp)
+		addi $sp, $sp, 4
 		jr $ra
 
-	#Array = $s0
-	#quickSort(low = $t5, high = $t6)
-	quickSort:
-		blt $t5, $t6, quickSort_ifTrue
-		j quickSort_ifFalse
-		quickSort_ifTrue:
-			jal partitionFunc
-			# pi = partitionFunc(low, high); pi = $t0
-			move $t0, $v0
-			# quickSort(low, pi - 1)
-			subi $t6, $t0, 1
-			jal quickSort
-			# quickSort(pi + 1, high)
-			addi $t5, $t0, 1
-			jal quickSort
-		quickSort_ifFalse:
-			jr $ra
+swap: #swap($a2, $a3)
+	move $t9, $a2
+	move $a2, $a3
+	move $a3, $t9
+	jr $ra
+
+#Array = $s0
+#quickSort(low = $a0, high = $a1)
+quickSort:
+	addi $sp, $sp, -12
+	# store arg in stack
+	sw $ra, 0($sp)
+	sw $a0, 4($sp)
+	sw $a1, 8($sp)
+	blt $a0, $a1, quickSort_ifTrue
+	j quickSort_end
+	quickSort_ifTrue:
+		jal partitionFunc
+		# pi = partitionFunc(low = a0, high = a1); pi = $t0
+		move $t0, $v0
+		# quickSort(low, pi - 1)
+		subi $a1, $t0, 1
+		jal quickSort
+		# quickSort(pi + 1, high)
+		addi $a0, $t0, 1
+		jal quickSort
+	quickSort_end:
+		lw $ra, 0($sp)
+		lw $a0, 4($sp)
+		lw $a1, 8($sp)
+		addi $sp, $sp, 12
+		jr $ra
 
 
 
